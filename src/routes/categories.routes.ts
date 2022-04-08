fed3693cc9d34ddc8218a961cdcca2792c219ecd
Kleverson Kenji <kleversonkenji@gmail.com>
2022-04-08 12:05:07 -0300

    solid srp and lsp principles usage

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,18 @@
 import { Router } from 'express';
 
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
-
-// chama constructor importado
 const categoriesRepository = new CategoriesRepository();
 
 // router do express
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  // validação
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: 'Category already exists!' });
-  }
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  categoriesRepository.create({ name, description });
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
